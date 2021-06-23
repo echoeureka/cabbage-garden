a54a2a7c7e7e9ef81fe95c0a1301a67f6425994d
cabbage9 <iherewithmyheart@gmail.com>
2021-06-23 19:00:43 +0800

    🎉 first commit

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,36 @@
+import { defineUserConfig } from 'vuepress-vite'
+import type { DefaultThemeOptions } from 'vuepress-vite'
+
+export default defineUserConfig<DefaultThemeOptions>({
+  bundler: '@vuepress/vite',
+  bundlerConfig: {},
+  lang: 'zh-CN',
+  title: '菜园子',
+  description: '园子里有一些菜',
+  head: [['link', { rel: 'icon', href: '/public/images/cabbage-dog-removebg.png' }]],
+  markdown: {
+    importCode: {
+      // handleImportPath: (str) =>
+      //   str.replace(/^@src/, path.resolve(__dirname, 'path/to/src'))
+    }
+  },
+  themeConfig: {
+    navbar: [
+      { text: '关于生活', link: '/life/' },
+      { text: '关于技术', link: '/tech/' }
+    ],
+    sidebar: {
+      '/tech/': [
+        {
+          isGroup: true,
+          text: '面试基础',
+          children: ['/tech/interview-js.md']
+        }
+      ]
+    },
+    logo: '/public/images/cabbage-dog-removebg.png',
+    darkMode: true,
+    repo: 'https://github.com/cabbage9/cabbage-garden',
+    repoLabel: 'GitHub'
+  }
+})
